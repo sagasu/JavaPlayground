d077e0a876604385625b646ab402e17501f247ca
sagasu <sagasu@gmail.com>
2016-02-07 21:41:14 +0000

    Adding simple controller to mvc application. Adding annotation based spring configuration. Adding missing dependencies and targeting build to war.

---
+++ spring-mvc/src/main/java/com/sagasu/myWebApp/controller/helloController.java
@@ -0,0 +1,17 @@
+package com.sagasu.myWebApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/greeting")
+    public String sayHello(Model model){
+
+        model.addAttribute("greeting", "hello world");
+
+        return "hello";
+    }
+}

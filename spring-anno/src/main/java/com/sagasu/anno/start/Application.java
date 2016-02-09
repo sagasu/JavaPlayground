946ed97a7e905e23bcd30d0dd2336a13d06906e1
sagasu <sagasu@gmail.com>
2016-02-09 23:42:31 +0000

    Renaming to keep convention of starting file.

--- spring-anno/src/main/java/com/sagasu/anno/start/Application.java
+++ spring-anno/src/main/java/com/sagasu/anno/start/Application.java
@@ -4,7 +4,7 @@ import com.sagasu.anno.service.CustomerService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Ap2 {
+public class Application {
     public static void main( String[] args )
     {
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");

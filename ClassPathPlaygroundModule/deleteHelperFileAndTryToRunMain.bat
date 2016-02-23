415294cb9326c32dd9f6967acfd6c2ba0b22aac8
sagasu <sagasu@gmail.com>
2016-02-23 21:53:04 +0000

    fixing classpath playground scripts

--- ClassPathPlaygroundModule/deleteHelperFileAndTryToRunMain.bat
+++ ClassPathPlaygroundModule/deleteHelperFileAndTryToRunMain.bat
@@ -2,6 +2,6 @@ del classes\main\java\com\sagasu\Helper.class
 
 REM this will not work
 REM java -cp classes main.java.com.sagasu.Main
-sdfsd
-REM this will work,
-java -cp classes;helper.jar main.java.com.sagasu.Main
+
+REM this will almost work - it will work if helper.jar was created in classes directory, so in it's structure it starts with main\java...
+java -cp classes;lib\helper.jar main.java.com.sagasu.Main
--- ClassPathPlaygroundModule/makeJarFromClass.bat
+++ ClassPathPlaygroundModule/makeJarFromClass.bat
@@ -1,1 +1,4 @@
-jar cvf helper.jar classes\main\java\com\sagasu\Main.class
+cd classes
+jar cvf helper.jar main\java\com\sagasu\Helper.class
+move helper.jar ../lib/helper.jar
+cd ..

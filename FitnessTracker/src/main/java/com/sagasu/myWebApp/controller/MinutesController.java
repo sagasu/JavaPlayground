97b555836861e1a426680b463c28f0689a65ba9a
sagasu <sagasu@gmail.com>
2016-02-23 13:25:44 +0000

    Fighting with jackson json to be able to use json and xml file.

--- FitnessTracker/src/main/java/com/sagasu/myWebApp/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/sagasu/myWebApp/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.sagasu.myWebApp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sagasu.myWebApp.model.Activity;
 import com.sagasu.myWebApp.model.Exercise;
 
 @Controller
@@ -15,6 +21,31 @@ public class MinutesController {
 		System.out.println("exercise: " + exercise.getMinutes());
 		
 		return "addMinutes";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities(){
+		List<Activity> activities = new ArrayList<Activity>();
+		
+		
+		
+		
+		Activity run = new Activity();
+		run.setDesc("Run");
+		activities.add(run);
+		
+		String desc = run.getDesc();
+		
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		activities.add(bike);
+		
+		Activity swim = new Activity();
+		swim.setDesc("Swim");
+		activities.add(swim);
+		
+		return activities;
+		
 	}
 	
 	@RequestMapping(value = "/addMinutesForward")

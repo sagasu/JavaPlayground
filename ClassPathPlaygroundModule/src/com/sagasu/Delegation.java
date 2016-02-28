03d5cc9269d199ff862a1da850dfd8f38d34d20e
sagasu <sagasu@gmail.com>
2016-02-28 11:30:12 +0000

    Delegation now displays all te jars, or folders that it is using for loading libriaries.

--- ClassPathPlaygroundModule/src/com/sagasu/Delegation.java
+++ ClassPathPlaygroundModule/src/com/sagasu/Delegation.java
@@ -8,7 +8,9 @@ public class Delegation {
     URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
     do{
       System.out.println(classLoader);
-
+      for(URL url : classLoader.getURLs()){
+        System.out.printf("\t %s\n", url.getPath());
+      }
     }while ((classLoader = (URLClassLoader) classLoader.getParent()) != null);
     System.out.println("Bootstrap classLoader");
   }

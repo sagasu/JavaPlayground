2240453f9e5d43ce42dbcf17c2010e4fe817c27f
Mateusz Kopij <sagasu@gmail.com>
2016-03-01 16:57:43 +0000

    minor refactoring. Extracting expected value to variable.

--- playground-default/src/test/java/com/sagasu/generics/GenericsPlaygroundTests.java
+++ playground-default/src/test/java/com/sagasu/generics/GenericsPlaygroundTests.java
@@ -9,7 +9,8 @@ public class GenericsPlaygroundTests {
 	@Test
 	public void setT_string_expectSetStringToBeReturned(){
 		GenericsPlayground<String> gps = new GenericsPlayground<String>();
-		gps.setT("foo");
+		String expectedString = "foo";
+		gps.setT(expectedString);
 		
 		Assert.assertEquals("foo", gps.getT());
 	}
@@ -17,11 +18,12 @@ public class GenericsPlaygroundTests {
 	@Test
 	public void setT_int_expectSetIntToBeReturned(){
 		GenericsPlayground<Integer> gps = new GenericsPlayground<Integer>();
-		gps.setT(1);
+		Integer expectedInt = 1;
+		gps.setT(expectedInt);
 		
 		// This is crazy, that: assertEquals(1, gps.getT())  doesn't work
 		// with: The method assertEquals(Object, Object) is ambiguous for the type 
-		assertEquals(Integer.valueOf(1), gps.getT());
+		assertEquals(Integer.valueOf(expectedInt), gps.getT());
 	}
 
 }

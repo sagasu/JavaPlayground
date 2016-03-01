2daf5f6ff7bec202e0c88961e1229b673fae6860
sagasu <sagasu@gmail.com>
2016-03-01 23:21:13 +0000

    making saving goal transactional - this is required, otherwise save doesn't work.

--- FitnessTracker/src/main/java/com/sagasu/myWebApp/service/GoalServiceImpl.java
+++ FitnessTracker/src/main/java/com/sagasu/myWebApp/service/GoalServiceImpl.java
@@ -2,6 +2,7 @@ package com.sagasu.myWebApp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sagasu.myWebApp.model.Goal;
 import com.sagasu.myWebApp.repository.GoalRepository;
@@ -12,6 +13,7 @@ public class GoalServiceImpl implements GoalService {
 	@Autowired
 	private GoalRepository goalRepository;
 	
+	@Transactional
 	public Goal save(Goal goal) {
 		return goalRepository.save(goal);
 	}

9ce9aa98751602060cb203c180ccef928669cfe0
sagasu <sagasu@gmail.com>
2016-03-03 22:37:51 +0000

    playing with phantom references

---
+++ MemoryManagementModule/src/main/java/phanRef/PhantomReferences.java
@@ -0,0 +1,40 @@
+package phanRef;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.util.ArrayList;
+
+public class PhantomReferences {
+
+    public static void main(String[] args){
+        ReferenceQueue<Person> queue = new ReferenceQueue<Person>();
+        ArrayList<FinalizePerson> list = new ArrayList<FinalizePerson>();
+        ArrayList<Person> people = new ArrayList<Person>();
+
+        for(int i = 0; i<10;i++){
+            Person p  = new Person();
+            people.add(p);
+            list.add(new FinalizePerson(p, queue));
+
+        }
+        people = null;
+        System.gc();
+
+        for (PhantomReference<Person> reference : list){
+            System.out.println(reference.isEnqueued());
+        }
+
+    }
+
+
+}
+
+class FinalizePerson extends PhantomReference<Person>{
+    public FinalizePerson(Person referent, ReferenceQueue<? super Person> q){
+        super(referent, q);
+    }
+
+    public void cleanup(){System.out.println("person is finalizing resources");}
+}
+
+class Person{}

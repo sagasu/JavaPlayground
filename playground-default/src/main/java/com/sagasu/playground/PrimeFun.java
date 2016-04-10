83a2e57818aa5e93d5d2103b53682d4f9129aa41
sagasu <sagasu@gmail.com>
2016-04-10 20:42:32 +0100

    Imperative and stream based implementation of a double of the first even number greater than 3.

--- playground-default/src/main/java/com/sagasu/playground/PrimeFun.java
+++ playground-default/src/main/java/com/sagasu/playground/PrimeFun.java
@@ -1,5 +1,8 @@
 package com.sagasu.playground;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -10,6 +13,8 @@ public class PrimeFun {
         IntStream.range(1,5).forEach(value -> System.out.println(isPrime2(value)));
         IntStream.range(1,5).forEach(value -> System.out.println(isPrime3(value)));
 
+        doubleOffirstEvenGreaterThanThree();
+        doubleOffirstEvenGreaterThanThree2();
     }
 
     private static boolean isPrime(final int number){
@@ -29,5 +34,33 @@ public class PrimeFun {
         Predicate<Integer> isDivisible = divisor -> number % divisor == 0;
 
         return number > 1 && IntStream.range(2, number).noneMatch(index -> isDivisible.test(index));
+    }
+
+    // find the double of the first even number greater than 3
+    private static void doubleOffirstEvenGreaterThanThree(){
+        // nice cast from array to list
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        int result = 0;
+        for(int e: values){
+            if(e > 3 && e % 2 == 0){
+                result = e * 2;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void doubleOffirstEvenGreaterThanThree2(){
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // sout shortcut converts to System.out.println
+        System.out.println(
+        values.stream()
+                .filter(e -> e > 3)
+                .filter(e -> e % 2 == 0)
+                .map(e -> e * 2)
+                .findFirst()
+                .get());
     }
 }

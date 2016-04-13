126dfca1bdc6e390e0cc7ed0687fd8e8211c0f5d
sagasu <sagasu@gmail.com>
2016-04-13 20:24:23 +0100

    playing with helper interfaces to handle situation when I want to use lambda and interface has multiple methods.

--- playground-default/src/main/java/com/sagasu/playground/MyFileFilter.java
+++ playground-default/src/main/java/com/sagasu/playground/MyFileFilter.java
@@ -1,6 +1,10 @@
 package com.sagasu.playground;
 
 
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FileFilter;
 
@@ -20,5 +24,26 @@ public class MyFileFilter {
 
         //A functional interface is an interface that has just one abstract method, and thus represents a single function contract.
         //If an interface has more than one method (it is not a funcitonal interface) and can not be implemented thru lambda.
+
+
+        // Using a helper interface when interface has multiple methods, but it is passed in a method, so the statement above still holds.
+        Component c = new Applet();
+        c.addMouseListener((ClickedListener)(e)->System.out.println("Clicked !"));
+
     }
+}
+
+interface ClickedListener extends MouseListener
+{
+    @Override
+    public default void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public default void mouseExited(MouseEvent e) {}
+
+    @Override
+    public default void mousePressed(MouseEvent e) {}
+
+    @Override
+    public default void mouseReleased(MouseEvent e) {}
 }

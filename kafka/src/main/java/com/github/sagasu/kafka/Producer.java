ef34a338ba75eda0455126921ead7a7c8e97a216
Mateusz Kopij <sagasu@gmail.com>
2020-06-29 17:30:18 +0100

    creating first producer

--- kafka/src/main/java/com/github/sagasu/kafka/Producer.java
+++ kafka/src/main/java/com/github/sagasu/kafka/Producer.java
@@ -1,7 +1,30 @@
 package com.github.sagasu.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Producer {
     public static void main(String[] args) {
-        System.out.println("jo");
+        Properties prop = new Properties();
+        String servers = "localhost:9092";
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(prop);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String,String>("first_topic", "hello");
+
+        producer.send(record);
+
+        //flush
+        producer.flush();
+
+        //flush and close
+        producer.close();
     }
 }
